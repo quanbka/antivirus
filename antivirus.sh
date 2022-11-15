e83b2a8dc2316e299ab1eab693094418f07cf066
Quan Le Van <quanbka.cntt@gmail.com>
2022-11-15 15:20:57 +0700

    anti virus

---
+++ antivirus.sh
@@ -0,0 +1,12 @@
+kill $(pgrep -f ./mysqld)
+kill $(pgrep -f /usr/bin/ps)
+kill $(pgrep -f /usr/bin/pythno)
+kill $(pgrep -f /usr/bin/bsd-port/knerl)
+kill $(pgrep -f /usr/bin/netstat)
+rm -rf   /var/tmp/.c/mysqld
+rm -rf   /usr/bin/ps
+rm -rf   /usr/bin/pythno
+rm -rf   /usr/bin/bsd-port/knerl
+rm -rf   /usr/bin/netstat
+yum reinstall -y procps  net-tools
+yum install -y procps  net-tools
